5d45836216f7ac625295ff36e3a694faf08af3d9
setKing <1330942316@qq.com>
2024-11-23 15:48:13 +0800

    更换gitee

--- user_srv/start.sh
+++ user_srv/start.sh
@@ -1,6 +1,6 @@
 srv_name="user_srv_server"
 
-chmod +x ./$srv_name
+chmod +x ./$srv_name.py
 #重启，如果已经存在则关闭重启
 if pgrep -x $srv_name > /dev/null
 then
@@ -9,13 +9,11 @@ then
     if ps -a | grep $srv_name | awk '{print $1}' | xargs kill $1
       then
         echo "${srv_name} starting"
-        cd user_srv
-        /root/.virtualenvs/mall_srv/bin/python3 server.py
+        /root/.virtualenvs/mall_srv/bin/python3 $srv_name.py --ip=192.168.194.100
         echo "${srv_name} started successfully."
     fi
 else
     echo "${srv_name} starting"
-    cd user_srv
-    /root/.virtualenvs/mall_srv/bin/python3 server.py
+    /root/.virtualenvs/mall_srv/bin/python3 $srv_name.py --ip=192.168.194.100
     echo "${srv_name} started successfully."
 fi
