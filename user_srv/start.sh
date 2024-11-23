e65b48e2b80eebc2a213054dcabd8b0a45fe6c50
setKing <1330942316@qq.com>
2024-11-23 16:38:03 +0800

    更换gitee

--- user_srv/start.sh
+++ user_srv/start.sh
@@ -6,12 +6,10 @@ if pgrep -x $srv_name > /dev/null
 then
     echo "${srv_name} is already running."
     echo "shutting down ${srv_name}"
-    if ps -a | grep $srv_name | awk '{print $1}' | xargs kill $1
-      then
-        echo "${srv_name} starting"
-        /root/.virtualenvs/mall_srv/bin/python3 $srv_name.py --ip=192.168.194.100
-        echo "${srv_name} started successfully."
-    fi
+    ps -ef | grep $srv_name | awk '{print $2}' | xargs kill $1
+    echo "${srv_name} starting"
+    /root/.virtualenvs/mall_srv/bin/python3 $srv_name.py --ip=192.168.194.100
+    echo "${srv_name} started successfully."
 else
     echo "${srv_name} starting"
     /root/.virtualenvs/mall_srv/bin/python3 $srv_name.py --ip=192.168.194.100
