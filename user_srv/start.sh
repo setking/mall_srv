36f0e87e4666c00bc113dc47bbe53693b40ca610
setKing <1330942316@qq.com>
2024-11-23 16:53:59 +0800

    更换gitee

--- user_srv/start.sh
+++ user_srv/start.sh
@@ -2,19 +2,17 @@ srv_name="user_srv_server"
 
 chmod +x ./$srv_name.py
 #重启，如果已经存在则关闭重启
-PIDS=`ps -ef | grep ${srv_name} | grep -v grep | awk 'print $2'`
+PIDS=`ps -ef | grep ${srv_name} | grep -v grep | awk '{print $2}'`
 if [ "$PIDS" != "" ];
 then
     echo "${srv_name} is already running."
     echo "shutting down ${srv_name}"
-    if ps -aux | grep $srv_name | awk '{print $2}' | xargs kill $1
-      then
-        echo "${srv_name} starting"
-        /root/.virtualenvs/mall_srv/bin/python3 $srv_name.py --ip=192.168.194.100
-        echo "${srv_name} started successfully."
-    fi
+    ps -aux | grep $srv_name | awk '{print $2}' | xargs kill $1
+    echo "${srv_name} starting"
+    /root/.virtualenvs/mall_srv/bin/python3 $srv_name.py --ip=192.168.194.100 > /dev/null 2>&1 &
+    echo "${srv_name} started successfully."
 else
     echo "${srv_name} starting"
-    /root/.virtualenvs/mall_srv/bin/python3 $srv_name.py --ip=192.168.194.100
+    /root/.virtualenvs/mall_srv/bin/python3 $srv_name.py --ip=192.168.194.100 > /dev/null 2>&1 &
     echo "${srv_name} started successfully."
 fi
